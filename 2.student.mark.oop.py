322c9eaea6e5564001c874b511a05548e155b6e4
sfdsfad <131836260+sfdsfad@users.noreply.github.com>
2024-03-12 23:40:45 +0700

    redo lab 2, create class Student

--- 2.student.mark.oop.py
+++ 2.student.mark.oop.py
@@ -1,79 +1,18 @@
 class Student:
-    def __init__(self, studentID, name, dob):
-        self.__studentID = studentID
-        self.__name = name
+    def __init__(self, student_name, student_id, dob):
+        self.__student_name = student_name
+        self.__student_id = student_id
         self.__dob = dob
-    
-    def setStudentID(self, studentID):
-        self.__studentID = studentID
 
-    def setName(self, name):
-        self.__name = name
-
-    def setDOB(self, dob):
-        self.__dob = dob
+    def get_student_name(self) -> str:
+        return self.__student_name
     
-    def getstudentid(self) -> str:
-        return self.__studentid
+    def get_student_id(self) -> str:
+        return self.__student_id
     
-    def getname(self) -> str:
-        return self.__name
-    
-    def getdob(self) -> str:
+    def get_dob(self) -> str:
         return self.__dob
-
-    def __str__(self) -> str:
-        return f"StudentID: {self.__studentID}, Name: {self.__name}, DOB: {self.__dob}."
-
-class Course:
-    def __init__(self, courseID, courseName):
-        self.__courseID = courseID
-        self.__name = courseName
-
-    def setCourseID (self, courseID):
-        self.__courseID = courseID
-
-    def setCourseName(self, courseName):
-        self.__name = courseName
-
-    def getCourseID(self) -> str:
-        return self.__courseID
     
-    def getName(self) -> str:
-        return self.__name
-
     def __str__(self) -> str:
-        return f"CourseID: {self.__courseID}, Course name: {self.__name}."
-
-class ManagementProgram:
-    def __init__(self):
-        self.students = []
-        self.courses = []
-        self.marks = {}
-
-    def input_num_of_students(self):
-        return int(input("Enter the number of students: "))
-
-    def input_one_student(self):
-        studentID = input("Enter the student ID: ")
-        name = input("Enter student name:")
-        dob = input("Enter student DOB: ")
-        return Student(studentID, name, dob)
-
-    def input_num_of_courses(self):
-        return int(input("Enter the number of courses"))
-
-    def input_one_course(self):
-        courseID = input("Enter course ID: ")
-        courseName = input("Enter course name: ")
-        return Course(courseID, courseName)
-
-    def input_student_mark(self):
-        marks = {}
-        print(f"Enter student's mark for {selected_course.name}: ") 
-        for student in students:
-            mark = float(input(f"Enter mark for {student.name} ({student.id}): "))
-            marks[student.id] = mark
-        return marks
-
+        return f"Student name: {self.__student_name}\nStudent ID: {self.__student_id}\nDate of birth: {self.__dob}"
     
